f34098c1b6b93cde7f7a01f57cfdbde310e541e9
Nick <nick.sjostrom12@gmail.com>
2015-08-11 21:13:35 -0400

    layout mirror support

--- Football_Scores/app/src/main/java/barqsoft/footballscores/ScoresWidgetProvider.java
+++ Football_Scores/app/src/main/java/barqsoft/footballscores/ScoresWidgetProvider.java
@@ -11,16 +11,24 @@ import android.widget.RemoteViews;
  */
 public class ScoresWidgetProvider extends AppWidgetProvider {
 
+    public static int scoreOne = 0;
+    public static int scoreTwo = 0;
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.scores_widget);
 
         remoteViews.setTextViewText(R.id.title, "This is a Widget");
+        remoteViews.setTextViewText(R.id.away_score_widget, Integer.toString(scoreOne));
+        remoteViews.setTextViewText(R.id.home_score_widget, Integer.toString(scoreTwo));
 
         pushWidgetUpdate(context, remoteViews);
     }
 
     public static void pushWidgetUpdate(Context context, RemoteViews remoteViews) {
+        scoreOne++;
+        scoreTwo++;
+
         ComponentName myWidget = new ComponentName(context, ScoresWidgetProvider.class);
         AppWidgetManager manager = AppWidgetManager.getInstance(context);
         manager.updateAppWidget(myWidget, remoteViews);
